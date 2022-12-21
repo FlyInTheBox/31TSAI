d135580aff37b1f59533d83e63553baf0213cd6b
FlyInTheBox <dt19wylazowskiw@gmail.com>
2022-12-21 12:32:26 +0100

    l

--- Zadanie 21.12.2022/index.php
+++ Zadanie 21.12.2022/index.php
@@ -6,18 +6,32 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kalendarz</title>
+    <style>
+        td {
+            border: 2px solid black;
+            padding: 20px;
+        }
+        table {
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 <body>
 <table>
     <tr>
         <?PHP
         $x = date("t");
-        for ($i = 1; $i < $x; $i++) {
-            echo "<td>" . $i . "</td>";
+        for ($i = 1; $i <= $x; $i++) {
             $z=$i%7;
-            if ($z=0){
+            if ($z!=0){
+                echo "<td>" . $i . "</td>";
+            }
+            else {
+                echo "<td>" . $i . "</td>";
                 echo "</tr><tr>";
             }
+
+
         }
         ?>
     </tr>
