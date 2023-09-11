79b722bd30aa93fc97f8e753664c7630d457e5ae
FlyInTheBox <d19wylazowskiw@gmail.com>
2023-09-11 13:56:00 +0200

    a

---
+++ ProjektowanieBazDanych/projekt1_v1.1.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="description"
+          content="Wylazowski Klasa 5D2">
+    <title>Strona Główna</title>
+    <style>
+        td {
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+<h2 align="center">Baza danych Wylazowski, Klasa 5D2</h2>
+<h3 align="center">Tu wstawiamy nasz kod</h3>
+
+<table cellspacing="0" cellpadding="0" border="1" style="width: 90%;" align="center">
+<tr bgcolor="#A000FF">
+    <td>ID</td>
+    <td>NAZWISKO</td>
+    <td>IMIĘ</td>
+    <td>USER ID</td>
+    <td>UWAGI</td>
+    <td>DATA REJESTRACJI</td>
+</tr>
+
+<?php
+$conn = mysqli_connect('localhost','root','','baza_testowa');
+$query = "SELECT emp.id,
+                     emp.first_name,
+                     emp.last_name
+                FROM emp
+               WHERE emp.last_name LIKE 'n%';";
+$result = mysqli_query($conn, $query);
+while ($row = mysqli_fetch_array($result))
+{
+    echo '<tr><td>';
+    printf("%d", $row['id']);
+    echo '</td><td>';
+    printf("%s", $row['last_name']);
+    echo '</td><td>';
+    printf("%s", $row['first_name']);
+    echo '</td></tr>';
+}
+?>
+</table>
+
+</body>
+</html>
