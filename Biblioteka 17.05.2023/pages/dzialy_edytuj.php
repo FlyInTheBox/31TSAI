9a34b674790fd79410e60da289b54b7a9e44064e
FlyInTheBox <d19wylazowskiw@gmail.com>
2023-09-15 13:52:09 +0200

    a

--- Biblioteka 17.05.2023/pages/dzialy_edytuj.php
+++ Biblioteka 17.05.2023/pages/dzialy_edytuj.php
@@ -2,11 +2,12 @@
 
 <?php
 $Id_dzial = $_GET['id'];
+$Nazwa = $_GET['Nazwa'];
 if ($_SERVER["REQUEST_METHOD"] == "POST") // Zapisz dane z formularza do bazy [INSERT]
 {
 
     $Nazwa = $_POST['Nazwa'];
-    $query = "UPDATE `dzialy` SET `Nazwa` = '".$Nazwa."' WHERE `dzialy`.`Id_dzial` = '.$Id_dzial.'";
+    $query = "UPDATE `dzialy` SET `Nazwa` = '".$Nazwa."' WHERE `dzialy`.`Id_dzial` = '$Id_dzial'";
     $result = mysqli_query($conn, $query);
     if ($result) {
         echo "Poprawnie zedytowano rekord";
@@ -14,10 +15,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") // Zapisz dane z formularza do bazy [I
         echo "Błąd edycji";
     }
 } else {
-    echo '<form action="?PAGE=dzialy_dodaj" method="post">
+    echo '<form action="?PAGE=dzialy_edytuj&id='.$Id_dzial.'&Nazwa='.$Nazwa.'" method="post">
        <table>
        <tr><td>Id_dzial</td><td><input type="text" name="Id_dzial" id="Id_dzial" value="'.$Id_dzial.'" disabled></td></tr>
-        <tr><td>Nazwa</td><td><input type="text" name="Nazwa" id="Nazwa"></td></tr>
+        <tr><td>Nazwa</td><td><input type="text" name="Nazwa" id="Nazwa" value="'.$Nazwa.'"></td></tr>
         <tr><td colspan="2" style="text-align: center"><button type="submit">Zapisz</button></td></tr>
        </table>
     </form>';}
