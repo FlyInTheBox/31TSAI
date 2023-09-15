739e46ff27b5a12d80868e6419834d4f6306723b
FlyInTheBox <d19wylazowskiw@gmail.com>
2023-09-15 13:17:41 +0200

    a

--- Biblioteka 17.05.2023/pages/dzialy.php
+++ Biblioteka 17.05.2023/pages/dzialy.php
@@ -9,7 +9,7 @@ if (mysqli_num_rows($result) > 0) {
     echo '<tr><th>Id_dzial</th><th>Nazwa</th><th><a class="add" href="?PAGE=dzialy_dodaj">&#10010;</a></th></tr>';
     while ($row = mysqli_fetch_assoc($result)) {
         echo '<tr><td>' . $row['Id_dzial'] . '</td><td>'
-            . $row['Nazwa'] . '</td><td><a href="dzialy_dodaj.php">edycja </a></td></tr>';
+            . $row['Nazwa'] . '</td><td><a href="?PAGE=dzialy_edytuj&id='.$row['Id_dzial'].'">edycja </a></td></tr>';
     }
     echo '</table>';
 } else {
--- Biblioteka 17.05.2023/pages/dzialy_edytuj.php
+++ Biblioteka 17.05.2023/pages/dzialy_edytuj.php
@@ -1,21 +1,22 @@
-<h1>Nowe dane tabeli działy</h1>
+<h1>Edytuj dane tabeli działy</h1>
 
 <?php
+$Id_dzial = $_GET['id'];
 if ($_SERVER["REQUEST_METHOD"] == "POST") // Zapisz dane z formularza do bazy [INSERT]
 {
+
     $Nazwa = $_POST['Nazwa'];
-    $query = "INSERT INTO dzialy (Nazwa) values ('".$Nazwa."')";
+    $query = "UPDATE `dzialy` SET `Nazwa` = '".$Nazwa."' WHERE `dzialy`.`Id_dzial` = '.$Id_dzial.'";
     $result = mysqli_query($conn, $query);
     if ($result) {
-        echo "Poprawnie dodano rekord do bazy";
+        echo "Poprawnie zedytowano rekord";
     } else {
-        echo "Błąd dodawania do bazy";
+        echo "Błąd edycji";
     }
-
 } else {
     echo '<form action="?PAGE=dzialy_dodaj" method="post">
        <table>
-       <tr><td>Id_dzial</td><td><input type="text" name="Id_dzial" id="Id_dzial" disabled></td></tr>
+       <tr><td>Id_dzial</td><td><input type="text" name="Id_dzial" id="Id_dzial" value="'.$Id_dzial.'" disabled></td></tr>
         <tr><td>Nazwa</td><td><input type="text" name="Nazwa" id="Nazwa"></td></tr>
         <tr><td colspan="2" style="text-align: center"><button type="submit">Zapisz</button></td></tr>
        </table>
